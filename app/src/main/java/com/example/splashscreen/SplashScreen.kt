0e93f94057ca16837d88255c93d554be845ae955
Devanshsati <thedevanshsati@gmail.com>
2024-04-21 14:00:31 +0530

    final

--- app/src/main/java/com/example/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/splashscreen/SplashScreen.kt
@@ -1,9 +1,11 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.splashscreen
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,8 +21,38 @@ class SplashScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        Handler(Looper.getMainLooper()).postDelayed({
-          startActivity(Intent(this, MainActivity::class.java))
-        }, 2000)
+//        Handler(Looper.getMainLooper()).postDelayed({
+//          startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 2000)
+        startHeavyTask()
+    }
+
+    private fun startHeavyTask() {
+        LongOperation().execute()
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private open inner class LongOperation : AsyncTask<String?, Void?, String>(){
+        @Deprecated("Deprecated in Java")
+        override fun doInBackground(vararg params: String?): String {
+            var value = true
+            while (value){
+                try {
+                    Thread.sleep(3000)
+                    value = false
+                }catch (e:Exception){
+                    Thread.interrupted()
+                }
+            }
+            return "Result"
+        }
+
+        @Deprecated("Deprecated in Java")
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val intent = Intent(this@SplashScreen,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
